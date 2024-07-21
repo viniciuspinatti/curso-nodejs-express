3d1ab0fdef99a2221aa11df9d55f7122208d0c39
viniciuspinatti <viniciuspinatti@hotmail.com>
2024-07-21 20:03:43 -0300

    Configured mondogb connection with mongoose

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,9 +1,15 @@
 import express from "express";
+import mongoose from 'mongoose'
 import routes from "./routes";
 
 class App {
   constructor() {
     this.server = express();
+
+    mongoose.connect('mongodb+srv://<user>:<password>@devhouse.r0abvsf.mongodb.net/?retryWrites=true&w=majority&appName=devhouse', {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    })
 
     this.middlewares();
     this.routes();
