8974e89b1d8f6c84bb868d672ad1874160ffe3cb
viniciuspinatti <viniciuspinatti@hotmail.com>
2024-07-23 16:57:12 -0300

    Created respective routes and entries in House controller to index, update and destroy

--- backend/src/routes.js
+++ backend/src/routes.js
@@ -10,6 +10,14 @@ const routes = new Router();
 const upload = multer(uploadConfig);
 
 routes.post("/sessions", SessionController.store);
+
 routes.post("/houses", upload.single("thumbnail"), HouseController.store);
+routes.get("/houses", HouseController.index);
+routes.put(
+  "/houses/:house_id",
+  upload.single("thumbnail"),
+  HouseController.update
+);
+routes.delete("/houses", HouseController.destroy);
 
 export default routes;
