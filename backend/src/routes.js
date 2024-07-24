b5f05d3dc1dfaf931f607abd428a4cd4b20254d7
viniciuspinatti <viniciuspinatti@hotmail.com>
2024-07-23 21:20:50 -0300

    Created dashboard controller to show all houses. Added cors package

--- backend/src/routes.js
+++ backend/src/routes.js
@@ -5,6 +5,7 @@ import uploadConfig from "./config/upload";
 
 import SessionController from "./controllers/SessionController";
 import HouseController from "./controllers/HouseController";
+import DashboardController from "./controllers/DashboardController";
 
 const routes = new Router();
 const upload = multer(uploadConfig);
@@ -19,5 +20,7 @@ routes.put(
   HouseController.update
 );
 routes.delete("/houses", HouseController.destroy);
+
+routes.get("/dashboard", DashboardController.show);
 
 export default routes;
