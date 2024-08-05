dfcc7971c02b47388ab960497d8f94186fb72b3d
viniciuspinatti <viniciuspinatti@hotmail.com>
2024-08-05 17:19:39 -0300

    Changing some packages due problems to run with node 14 and sequelize < 8. Configured database migrations and created user model loading the sequelize postgres instance

--- tasklist/src/routes.js
+++ tasklist/src/routes.js
@@ -1,11 +1,15 @@
 import { Router } from "express";
+import User from "./app/models/User";
 
 const routes = new Router();
 
-routes.get("/test", (req, res) => {
-  return res.json({
-    ok: true,
+routes.get("/test", async (req, res) => {
+  const user = await User.create({
+    name: "vinicius",
+    email: "vinicius@test.com",
+    password_hash: "123",
   });
+  return res.json(user);
 });
 
 export default routes;
