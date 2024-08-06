6303caba0255c877e052bcd14dbfc7e5132f86ca
viniciuspinatti <viniciuspinatti@hotmail.com>
2024-08-06 09:56:28 -0300

    Removed comment in docker-compose file. Created user controller to with store user action. Added bcryptjs to encrypt password

--- tasklist/src/routes.js
+++ tasklist/src/routes.js
@@ -1,15 +1,9 @@
 import { Router } from "express";
-import User from "./app/models/User";
+
+import UserController from "./app/controllers/UserController";
 
 const routes = new Router();
 
-routes.get("/test", async (req, res) => {
-  const user = await User.create({
-    name: "vinicius",
-    email: "vinicius@test.com",
-    password_hash: "123",
-  });
-  return res.json(user);
-});
+routes.post("/users", UserController.store);
 
 export default routes;
