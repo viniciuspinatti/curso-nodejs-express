068721f6a5931e4cd72d19c926e4144f8924434d
viniciuspinatti <viniciuspinatti@hotmail.com>
2024-08-09 11:42:52 -0300

    Created middleware that verify the token and injects it on req object using userId attribute

--- tasklist/src/routes.js
+++ tasklist/src/routes.js
@@ -1,5 +1,7 @@
 import { Router } from "express";
 
+import authMiddleware from "./app/middlewares/auth";
+
 import UserController from "./app/controllers/UserController";
 import SessionController from "./app/controllers/SessionController";
 
@@ -8,5 +10,10 @@ const routes = new Router();
 routes.post("/users", UserController.store);
 
 routes.post("/sessions", SessionController.store);
+
+// All routes bellow will apply this middleware
+routes.use(authMiddleware);
+
+routes.put("/users", UserController.update);
 
 export default routes;
